63edee9b9293acc0b478330e44d6aff4eada5343
wang jiahao <wangjiahao@koolearn.com>
2018-05-16 15:42:29 +0800

    启动检测

--- worker/src/main/java/cn/donut/ordermq/web/TestCtrl.java
+++ worker/src/main/java/cn/donut/ordermq/web/TestCtrl.java
@@ -1,22 +1,10 @@
 package cn.donut.ordermq.web;
 
-import cn.donut.ordermq.entity.MqAttachments;
-import cn.donut.ordermq.entity.MqInformation;
-import cn.donut.ordermq.service.MqAttachmentsServiceProvider;
-import cn.donut.ordermq.service.MqInformationServiceProvider;
-import com.alibaba.fastjson.JSONException;
-import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import java.nio.charset.Charset;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author wangjiahao
@@ -26,11 +14,11 @@ import java.util.Map;
 public class TestCtrl {
 //
     private static final Logger log = LoggerFactory.getLogger(TestCtrl.class);
-//
-    @Autowired
-    private MqInformationServiceProvider mqInformationServiceProvider;
-    @Autowired
-    private MqAttachmentsServiceProvider mqAttachmentsServiceProvider;
 
+    @RequestMapping("/health")
+    @ResponseBody
+    public String health(){
+        return "{STATUS:UP!}";
+    }
 
 }
