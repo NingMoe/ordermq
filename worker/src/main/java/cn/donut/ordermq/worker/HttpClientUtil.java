8570291609442aa763fbe9f1d23dc74bb7add132
LiYuAn <liyuan@koolearn.com>
2018-05-21 10:19:46 +0800

    删除ssl方法

--- worker/src/main/java/cn/donut/ordermq/worker/HttpClientUtil.java
+++ worker/src/main/java/cn/donut/ordermq/worker/HttpClientUtil.java
@@ -20,10 +20,6 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContextBuilder;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -33,14 +29,7 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLException;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocket;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.security.GeneralSecurityException;
-import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -218,134 +207,6 @@ public class HttpClientUtil {
             }
         }
         return httpStr;
-    }
-
-    /**
-     * 发送 SSL POST 请求（HTTPS），K-V形式
-     *
-     * @param apiUrl API接口URL
-     * @param params 参数map
-     * @return
-     */
-    public static String doPostSSL(String apiUrl, Map<String, Object> params) {
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(createSSLConnSocketFactory()).setConnectionManager(connMgr).setDefaultRequestConfig(requestConfig).build();
-        HttpPost httpPost = new HttpPost(apiUrl);
-        CloseableHttpResponse response = null;
-        String httpStr = null;
-
-        try {
-            httpPost.setConfig(requestConfig);
-            List<NameValuePair> pairList = new ArrayList<NameValuePair>(params.size());
-            for (Map.Entry<String, Object> entry : params.entrySet()) {
-                NameValuePair pair = new BasicNameValuePair(entry.getKey(), entry
-                        .getValue().toString());
-                pairList.add(pair);
-            }
-            httpPost.setEntity(new UrlEncodedFormEntity(pairList, Charset.forName("utf-8")));
-            response = httpClient.execute(httpPost);
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode != HttpStatus.SC_OK) {
-                return null;
-            }
-            HttpEntity entity = response.getEntity();
-            if (entity == null) {
-                return null;
-            }
-            httpStr = EntityUtils.toString(entity, "utf-8");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (response != null) {
-                try {
-                    EntityUtils.consume(response.getEntity());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return httpStr;
-    }
-
-    /**
-     * 发送 SSL POST 请求（HTTPS），JSON形式
-     *
-     * @param apiUrl API接口URL
-     * @param json   JSON对象
-     * @return
-     */
-    public static String doPostSSL(String apiUrl, String json) {
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(createSSLConnSocketFactory()).setConnectionManager(connMgr).setDefaultRequestConfig(requestConfig).build();
-        HttpPost httpPost = new HttpPost(apiUrl);
-        CloseableHttpResponse response = null;
-        String httpStr = null;
-
-        try {
-            httpPost.setConfig(requestConfig);
-            StringEntity stringEntity = new StringEntity(json.toString(), "UTF-8");//解决中文乱码问题
-            stringEntity.setContentEncoding("UTF-8");
-            stringEntity.setContentType("application/json");
-            httpPost.setEntity(stringEntity);
-            response = httpClient.execute(httpPost);
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode != HttpStatus.SC_OK) {
-                return null;
-            }
-            HttpEntity entity = response.getEntity();
-            if (entity == null) {
-                return null;
-            }
-            httpStr = EntityUtils.toString(entity, "utf-8");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (response != null) {
-                try {
-                    EntityUtils.consume(response.getEntity());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return httpStr;
-    }
-
-    /**
-     * 创建SSL安全连接
-     *
-     * @return
-     */
-    private static SSLConnectionSocketFactory createSSLConnSocketFactory() {
-        SSLConnectionSocketFactory sslsf = null;
-        try {
-            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
-
-                public boolean isTrusted(X509Certificate[] chain, String authType) {
-                    return true;
-                }
-            }).build();
-            sslsf = new SSLConnectionSocketFactory(sslContext, new X509HostnameVerifier() {
-
-
-                public boolean verify(String arg0, SSLSession arg1) {
-                    return true;
-                }
-
-
-                public void verify(String host, SSLSocket ssl) throws IOException {
-                }
-
-
-                public void verify(String host, X509Certificate cert) throws SSLException {
-                }
-
-
-                public void verify(String host, String[] cns, String[] subjectAlts) throws SSLException {
-                }
-            });
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
-        return sslsf;
     }
 
 
