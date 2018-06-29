445f2504956ef2c134931ca8270ef5fa40e28c98
wang jiahao <wangjiahao@koolearn.com>
2018-06-29 12:02:28 +0800

    使用线程池

--- worker/src/main/java/cn/donut/ordermq/worker/order/OrderPaySuccessReceiver.java
+++ worker/src/main/java/cn/donut/ordermq/worker/order/OrderPaySuccessReceiver.java
@@ -3,6 +3,8 @@ package cn.donut.ordermq.worker.order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.nio.charset.Charset;
 
@@ -13,12 +15,19 @@ import java.nio.charset.Charset;
  */
 @Slf4j
 public class OrderPaySuccessReceiver implements MessageListener {
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
 
     @Override
-    public void onMessage(Message message) {
-        log.info("收到消息：==>{}" + message.toString());
-        String json = new String(message.getBody(), Charset.defaultCharset());
-        //TODO:具体处理
-        System.out.println("json = " + json);
+    public void onMessage(final Message message) {
+        taskExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                log.info("收到消息：==>{}" + message.toString());
+                String json = new String(message.getBody(), Charset.defaultCharset());
+                //TODO:具体处理
+                System.out.println("json = " + json);
+            }
+        });
     }
 }
