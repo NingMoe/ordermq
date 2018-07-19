13fdcc9b60ce5771a7eae255accb5dd2dbc7411f
LiYuAn <liyuan@koolearn.com>
2018-07-19 14:23:03 +0800

    更改配置文件

--- worker/src/main/java/cn/donut/ordermq/worker/order/OrderRefundReceiver.java
+++ worker/src/main/java/cn/donut/ordermq/worker/order/OrderRefundReceiver.java
@@ -71,9 +71,11 @@ public class OrderRefundReceiver implements MessageListener {
                 //转换
                 MqOrderInfo orderInfo = mqUtil.Json2Order(json);
                 //是否多纳订单
-                //是否多纳订单
                 Map<String, Object> map = iOrderService.checkProLine(orderInfo);
                 Boolean flag = false;
+                if (null != map && map.containsKey("flag")) {
+                    flag = (Boolean) map.get("flag");
+                }
                 if (orderInfo != null && flag) {
                     //保存
                     MqRecord mqRecord = mqUtil.saveMsg(json, "order.refund");
