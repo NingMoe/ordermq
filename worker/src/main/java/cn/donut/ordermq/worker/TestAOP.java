2b14bb5e64a71772afc499b43b6b338cdd5752b0
LiYuAn <liyuan@koolearn.com>
2018-07-26 15:09:36 +0800

    开启aop

--- worker/src/main/java/cn/donut/ordermq/worker/TestAOP.java
+++ worker/src/main/java/cn/donut/ordermq/worker/TestAOP.java
@@ -1,38 +1,38 @@
-//package cn.donut.ordermq.worker;
-//
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @author wangjiahao
-// * @date 2018/7/20 9:25
-// */
-//@Component
-//@Aspect
-//public class TestAOP {
-//
-//    private static final Logger log = LoggerFactory.getLogger(TestAOP.class);
-//
-//    @Pointcut("execution(* cn.donut.ordermq.worker.order.OrderRefundReceiver.*(..))")
-//    public void pointcut() {}
-//
-//
-//
-//    @Around("pointcut()")
-//    public void around(ProceedingJoinPoint jp){
-//        System.out.println("=============before================");
-//        try {
-//            jp.proceed();
-//            System.out.println("=============around================");
-//        } catch (Throwable throwable) {
-//            System.out.println("=============exception================");
-//        }
-//        System.out.println("=============after================");
-//    }
-//
-//}
+package cn.donut.ordermq.worker;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wangjiahao
+ * @date 2018/7/20 9:25
+ */
+@Component
+@Aspect
+public class TestAOP {
+
+    private static final Logger log = LoggerFactory.getLogger(TestAOP.class);
+
+    @Pointcut("execution(* cn.donut.ordermq.worker.order.OrderRefundReceiver.pushAop(..))")
+    public void pointCut() {
+    }
+
+    @Around("pointCut()")
+    public void around(ProceedingJoinPoint jp) throws Throwable {
+        System.out.println("------------------before-------------------------");
+        try {
+            System.out.println("------------------around-------------------------");
+            jp.proceed();
+        } catch (Exception e) {
+            log.error("异常！", e);
+            System.out.println("------------------exception-------------------------");
+        }
+        System.out.println("------------------after-------------------------");
+    }
+
+}
